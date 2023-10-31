22d09cbd7d101d7e5ad1f7dfbbeea6961d4d807d
Henrique Damasceno <henriquim25@yahoo.com.br>
2023-10-31 01:48:38 -0300

    feat: implemented routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const category = {
+    name,
+    description,
+    id: uuid(),
+  };
+
+  categories.push(category);
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createCouse } from './routes';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get('/', createCouse);
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('Servidor Rodando');
