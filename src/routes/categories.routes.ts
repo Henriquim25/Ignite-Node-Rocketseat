7b93066a8d36e64604362f2d20487d8867917669
Henrique Damasceno <henriquim25@yahoo.com.br>
2023-11-02 17:16:17 -0300

    adding services for category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,22 @@
 import { Router, response } from 'express';
-import { Category } from '../model/Category';
+import { CategoryRepository } from '../repositories/CategoryRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
+  const createCategoryService = new CreateCategoryService(categoryRepository);
 
-  const category = new Category();
+  createCategoryService.execute({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  return response.status(201).send();
+});
 
-  categories.push(category);
-  return response.status(201).json({ category });
+categoriesRoutes.get('/', (request, response) => {
+  const list = categoryRepository.list();
+  return response.status(201).json(list);
 });
 
 export { categoriesRoutes };
